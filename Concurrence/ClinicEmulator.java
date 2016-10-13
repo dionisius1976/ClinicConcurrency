dc60ac6996b40fd626fe975fcb18edeb54ec441d
Dionisius <maslo111@rambler.ru>
2016-10-13 23:05:54 +0300

    Update ClinicEmulator.java

--- Concurrence/ClinicEmulator.java
+++ Concurrence/ClinicEmulator.java
@@ -1,18 +1,16 @@
 package ru.lesson.lessons.ConcurrenceClinic;
-
 import ru.lesson.lessons.ClinicProject.ClinicSimpleArrayList;
 
 class ClinicEmulator {
 
     public static void main(String[] args) throws InterruptedException {
         ClinicSimpleArrayList cl = new ClinicSimpleArrayList();
-		//Запуск админа
+	//Запуск админа
         Admin admin = new Admin(cl);
         Thread.sleep(1000);
         cl.printClientsDataBase();
         System.out.println();
-
-		//создание и запуск 4 потоков User
+	//создание и запуск 4 потоков User
         for (int i = 0; i < 4; i++) {
             String userName = "User"+i;
             new User(cl, i, userName);
